1e4ac821821dea0692bd08e6eeef4d2ba186d7e9
github username <github email address>
2019-12-15 17:44:49 -0600

    added input form

--- pingpong/src/index.js
+++ pingpong/src/index.js
@@ -5,6 +5,7 @@ let game = () => {
 let p1Score = parseInt(document.getElementsByClassName('player-1-score')[0].innerHTML)
 let p2Score = parseInt(document.getElementsByClassName('player-2-score')[0].innerHTML)
 
+
 let p1ScoreDiv = document.getElementsByClassName('player-1-score')[0]
 let p2ScoreDiv = document.getElementsByClassName('player-2-score')[0]
 let winnerDiv = document.getElementsByClassName('winner')[0]
@@ -14,35 +15,47 @@ let p1WinCount = 0
 let p2WinCount = 0
 
 
-// This is the first function initiated. It grabs each button and sets the limit.  
-//  I made collection because I could not iterate through HTML collection with forEach(), but I found this online.
-function startGame() {
-    collection = document.getElementsByClassName('btn btn-primary')
-    Array.from(collection).forEach(function (button) {
-     button.addEventListener('click', e => {
-      keepscore(`${parseInt(button.innerText)}`)     
-     })
-  })
-  
+// This is the first function initiated. It grabs the value from each input field and passes them
+// to the keepScore function
+
+
+function initiateGame() {
+  console.log('game started')
+  let submitButton = document.querySelector('#submit')
+  submitButton.addEventListener('click', e => {
+    console.log('button clicked')
+    let p1name = document.querySelector('#player1name').value
+    let p2name = document.querySelector('#player2name').value
+    let scoreLimit = parseInt(document.querySelector('#scoreLimit').value)
+    keepscore(scoreLimit, p1name, p2name)
+    
+  }) 
    
- }
+  }
+
+
+
+
 
 // This function is only initiated after a button is clicked to set the score limit. It envokes
 // all the helper methods below this one. 
-function keepscore(limit) {
-  collection = document.getElementsByClassName('btn btn-primary')
-    Array.from(collection).forEach(function (button) {
-    button.remove()
-  })
-  document.querySelector('.choose-limit').remove()
-
+function keepscore(limit, p1name, p2name) {
+  console.log('--------------------------------------------------------')
+  document.querySelector('.user-form').remove()
+  
   displayLimit = document.querySelector('.display-limit')
-
   displayLimit.innerText = `Score ${limit} points to win!`
+  // <div class='player1-name-score'> </div>
+  let p1ns = document.querySelector('.player1-name-score')
+  let p2ns = document.querySelector('.player2-name-score') 
+  p1ns.innerText = `${p1name}'s score:`
+  p2ns.innerText = `${p2name}'s score:`
+
+
  
 document.addEventListener("keyup", e => {
-  if (e.key == 'ArrowLeft') { p1ScoreUp(limit)
-  } else if (e.key == 'ArrowRight'){ p2ScoreUp(limit)
+  if (e.key == 'ArrowLeft') { p1ScoreUp(limit, p1name, p2name)
+  } else if (e.key == 'ArrowRight'){ p2ScoreUp(limit, p1name, p2name)
   } else if (e.key == ','){ p1ScoreDown()
   } else if (e.key == '.'){ p2ScoreDown()
   } else {
@@ -52,17 +65,17 @@ document.addEventListener("keyup", e => {
 
 // <---------------------------------HELPER METHODS----------------------------------------->
 
-function p1ScoreUp(limit){
+function p1ScoreUp(limit, p1name, p2name){
   clearWinnerDiv()
   p1Score += 1
   console.log(`The difference is ${p1Score - p2Score}`)
   if (p1Score >= limit){
     if (p1Score - p2Score >= 2) {
-    winnerDiv.innerHTML = "<u>Player 1 wins this set!</u>"
+    winnerDiv.innerHTML = `<u>${p1name} wins this set!</u>`
     p1Score = 0
     p2Score = 0
     p2ScoreDiv.innerHTML = p2Score
-    winCount("player1")}
+    winCount("player1", p1name, p2name)}
     else {
       winnerDiv.innerText = 'You must win by 2!'
     }
@@ -75,16 +88,16 @@ function p1ScoreDown(){
   p1ScoreDiv.innerHTML = p1Score
 }
 
-function p2ScoreUp(limit){
+function p2ScoreUp(limit, p1name, p2name){
   clearWinnerDiv()
   p2Score += 1
   if (p2Score >= limit){
     if (p2Score - p1Score >= 2){
-    winnerDiv.innerHTML = '<u>Player 2 wins this set!</u>'
+    winnerDiv.innerHTML = `<u>${p2name} wins this set!</u>`
     p2Score = 0
     p1Score = 0
     p1ScoreDiv.innerHTML = p1Score
-    winCount("player2")}
+    winCount("player2", p1name, p2name)}
     else {
       winnerDiv.innerText = 'You must win by 2!'
      }
@@ -104,19 +117,20 @@ function clearWinnerDiv() {
 
 
 // <----this function is called when a player reaches the limit and their win count goes up---->
-function winCount(winner) {
+function winCount(winner, p1name, p2name) {
   if (winner == 'player1'){
     p1WinCount += 1
-    p1WinCountDiv.innerText = `Player 1 win count: ${p1WinCount}`
+    p1WinCountDiv.innerText = `${p1name}'s win count: ${p1WinCount}`
   } else if (winner == 'player2') {
     p2WinCount += 1
-    p2WinCountDiv.innerText = `Player 2 win count: ${p2WinCount}`
+    p2WinCountDiv.innerText = `${p2name}'s win count: ${p2WinCount}`
   }
 }
   }
 
   //  <-------this is like the run fuction we stored all the steps in run--------->
-  startGame()
+  // startGame()
+  initiateGame() 
   
   
 }
